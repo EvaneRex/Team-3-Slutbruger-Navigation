c84e7f7efa99fc41c763848127f9518df9f70bbf
Rikke Rasmussen <RiMaRa@outlook.dk>
2025-12-12 09:58:56 +0100

    js fake login

---
+++ script.js
@@ -0,0 +1,48 @@
+// Bruger klasse
+class User {
+  #username = "";
+
+  constructor(loginFormId, headerUsernameId) {
+    this.loginForm = document.getElementById(loginFormId);
+    this.headerUsername = document.getElementById(headerUsernameId);
+    this.initLogin();
+  }
+
+  initLogin() {
+    this.loginForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const username = document.getElementById("usernameLogin").value;
+      const password = document.getElementById("password").value;
+
+      // Fake brugernavn
+      if (username === "Gruppe6" && password === "Eksamen") {
+        this.#username = username;
+        this.updateHeader();
+        this.hideLogin();
+      } else {
+        alert("Forkert brugernavn eller adgangskode");
+      }
+    });
+  }
+  // Smider brugernavnet op i headeren
+  updateHeader() {
+    this.headerUsername.textContent = this.#username;
+  }
+
+  hideLogin() {
+    this.loginForm.style.display = "none";
+
+    // lægger et overlay på kortet
+    const maps = document.getElementById("map");
+    maps.classList.remove("blur");
+  }
+
+  getUsername() {
+    return this.#username;
+  }
+}
+// Kører klassen
+document.addEventListener("DOMContentLoaded", () => {
+  new User("login", "username");
+});
