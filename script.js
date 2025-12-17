53eeccd83869e733528432f0262d7c165514683b
Signeg12 <146083058+Signeg12@users.noreply.github.com>
2025-12-17 17:38:09 +0100

    rettet

--- script.js
+++ script.js
@@ -397,7 +397,6 @@ document.getElementById("nextTaskBtn").addEventListener("click", () => {
   if (
     document.getElementById("nextTaskBtn").textContent === "Tilbage til kort"
   ) {
-    const completedScenario = activeScenario;
 
     showExploreUI();
     introTxtScreen("loggedIn");
@@ -413,12 +412,8 @@ document.getElementById("nextTaskBtn").addEventListener("click", () => {
     activeScenario = null;
     activeTaskIndex = null;
     lockedTask = false;
-
-    renderScenarioList();
     return;
   }
-
-  let error = document.getElementById("taskError");
 
   const hasCheckboxes = document.querySelector(
     "#taskOptions input[type='checkbox']"
